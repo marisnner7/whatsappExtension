f4da396b25508cee2883c6e7e0362de9c5a608b1
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-06-23 11:16:48 -0300

    added logic to create the btn and functions

---
+++ content.js
@@ -0,0 +1,23 @@
+const interval = setInterval(() => {
+  const header = document.querySelector("._1QUKR");
+  if(header){
+    console.log(header)
+    clearInterval(interval)
+  }
+
+  const button = document.createElement("button")
+  button.innerHTML = "2x"
+  button.classList.add("twoTimesButton")
+
+
+  button.addEventListener("click",() => {
+    const audios = document.querySelectorAll("audio")
+      audios.forEach((audio)=>{
+        audio.playbackRate = 2;
+      })
+  })
+  header.appendChild(button)
+
+
+}, 1000);
+
